93243b434ce27c264da465a13d5129e39a880685
Jake Payne <paynekid96@gmail.com>
2021-12-16 16:38:54 -0700

    added displayed categories, and added banner

--- react-app/src/components/Home/home.js
+++ react-app/src/components/Home/home.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
 import Hangouts from "../hangouts/Hangouts";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllHangouts } from "../../store/hangouts";
+import "./home.css";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const hangouts = useSelector((state) => Object.values(state.hangouts));
+  console.log("hangouts", hangouts);
+  const categories = new Set();
+  hangouts?.map(({ category }) => categories.add(category));
+  const catArr = Array.from(categories);
+
+  useEffect(() => {
+    dispatch(getAllHangouts());
+  }, [dispatch]);
   return (
     <div className="page">
-      <div className="banner"></div>
-      <div className="categories"></div>
+      <div className="banner">
+        <h1 className="welcome">Live Hangout</h1>
+      </div>
+      <div className="categories">
+        {catArr?.map((category) => (
+          <div className="category">{category}</div>
+        ))}
+      </div>
       <div className="main-container">
-        <Hangouts />
+        <Hangouts hangouts={hangouts} />
       </div>
       <div className="see-more"></div>
     </div>
--- react-app/src/components/hangouts/Hangouts.js
+++ react-app/src/components/hangouts/Hangouts.js
@@ -3,13 +3,9 @@ import { getAllHangouts } from "../../store/hangouts";
 import { useDispatch, useSelector } from "react-redux";
 import "./Hangouts.css";
 
-function Hangouts() {
+function Hangouts({ hangouts }) {
   const dispatch = useDispatch();
-  const hangouts = useSelector((state) => Object.values(state.hangouts));
 
-  useEffect(() => {
-    dispatch(getAllHangouts());
-  }, [dispatch]);
 
   return (
     <div className="hangout-container">
@@ -29,14 +25,13 @@ function Hangouts() {
           bookmarks,
         }) => (
           <div key={id} className="hangout">
-            <img src={image} alt="hangout_photo"></img>
+            <img src={image} className="photo" alt="hangout_photo"></img>
             <div className="info">
               <span>{title}</span>
               <span>Hosted by: {host}</span>
               <span>Open for RSVP: {open.toString()}</span>
               {open && <span>RSVPs: {rsvps.length}</span>}
               {!open && <span>Bookmarks: {bookmarks.length}</span>}
-              <span>{description}</span>
               <span>
                 Starts on:{startDate.slice(0, 17)} Ends on:
                 {endDate.slice(0, 17)}
